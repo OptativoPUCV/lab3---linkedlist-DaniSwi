485c093117876042a0e6c8d0358c3f4bc085bf9b
Your Name <you@example.com>
2024-03-26 22:28:24 +0000

    new try

--- list.c
+++ list.c
@@ -69,12 +69,18 @@ void *prevList(List * list) { //La segunda función retorna el dato del nodo ant
 }
 
 void pushFront(List *list, void *data) {
-  Node *newNode = createNode(data);
+  /* Node *newNode = createNode(data);
   newNode->next = list->head;
   if(list->head) {
     list->head->prev = newNode;
     newNode->next = list->head;
   }
+  list->head = newNode; */
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->prev = NULL;
+  if(list->head)
+    list->head->prev = newNode;
   list->head = newNode;
 }
 
